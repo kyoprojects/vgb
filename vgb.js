4d36f48f7edbcf3b6f615311ebd67d5e9ecced91
sleakchat <support@sleak.chat>
2024-09-13 21:51:27 +0200

    chore: Add scroll event listener and requestAnimationFrame loop

--- vgb.js
+++ vgb.js
@@ -1,1 +1,14 @@
+console.log('hello world');
 
+const lenis = new Lenis();
+
+lenis.on('scroll', e => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);

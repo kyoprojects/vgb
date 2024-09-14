76cd0cbcedbe5efdfe8241923ee4058f68680a92
sleakchat <support@sleak.chat>
2024-09-14 10:10:23 +0200

    Optimize scroll event handling and animation in vgb.js

--- vgb.js
+++ vgb.js
@@ -12,24 +12,21 @@ window.addEventListener('DOMContentLoaded', function () {
   // header
   // let targetElement = document.querySelector(".framer-1jbc6qn");
   let targetElement = document.querySelector('.header-wrap');
-  //   gsap.to(targetElement, { duration: 0.3, y: 0, autoAlpha: 0 });
   let lastScrollTop = 0;
   let threshold = 100;
   let hysteresis = 100;
-
   window.addEventListener(
     'scroll',
     () => {
       let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
       if (currentScroll > lastScrollTop) {
-        // Downscroll
+        // downscroll
         if (currentScroll > threshold + hysteresis) {
-          // Adding hysteresis
+          // hysteresis
           gsap.to(targetElement, { duration: 0.3, y: -80, autoAlpha: 0 });
         }
       } else {
-        // Upscroll
+        // upscroll
         if (currentScroll > threshold + hysteresis) {
           gsap.to(targetElement, { duration: 0.3, y: 0, autoAlpha: 1 });
         }
@@ -38,4 +35,37 @@ window.addEventListener('DOMContentLoaded', function () {
     },
     false
   );
+
+  // split text
+  let fullText;
+  function runSplit() {
+    let textElement = document.querySelector('.primary-heading');
+    fullText = new SplitType(textElement, { types: 'lines' });
+    console.log(fullText);
+
+    $('.line').append('<div class="line-mask"></div>');
+  }
+  runSplit();
+  window.addEventListener('resize', () => {
+    fullText.revert();
+    runSplit();
+  });
+  $('.line').each(function () {
+    let triggerElement = $(this);
+    let targetElement = triggerElement.find('.line-mask');
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerElement,
+        start: 'top bottom',
+        end: 'bottom 20%',
+        scrub: true,
+        delay: 10
+      }
+    });
+    tl.to(targetElement, {
+      width: '0%',
+      // duration: 1,
+      ease: 'power2.inOut'
+    });
+  });
 });
